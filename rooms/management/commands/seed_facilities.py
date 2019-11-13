ab440109970d55d520706a7ba6b117f6f96bc475
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-11-13 22:11:22 +0900

    seed함수 생성

---
+++ rooms/management/commands/seed_facilities.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from rooms.models import Facility
+
+
+class Command(BaseCommand):
+
+    help = "This command create facilities"
+
+    def handle(self, *args, **options):
+        facilities = [
+            "Private entrance",
+            "Paid parking on premises",
+            "Paid parking off premises",
+            "Elevator",
+            "Parking",
+            "Gym",
+        ]
+
+        for f in facilities:
+            Facility.objects.create(name=f)
+        
+        self.stdout.write(self.style.SUCCESS("Facilities created!"))
+
